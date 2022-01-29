e2e9126a8bff284b03118a9fc3a944134483fcc8
timmy-pelumy <timileyinpelumi.dev@gmail.com>
2022-01-29 12:01:36 +0100

    Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   middlewares.js
    	new file:   public/user_f.png
    	new file:   public/user_m.png
    	modified:   views/base.ejs

--- middlewares.js
+++ middlewares.js
@@ -5,6 +5,16 @@ function context(req, res, next) {
 	res.locals.sitename = process.env.SITENAME;
 	res.locals.user = req.user || null;
 
+	if (req.user) {
+		if (req.user.gender === 'male') {
+			res.locals.avatar = '/user_m.png';
+		} else if (req.user.gender === 'female') {
+			res.locals.avatar = '/user_f.png';
+		}
+	} else {
+		res.locals.avatar = '/user_m.png';
+	}
+
 	next();
 }
 
