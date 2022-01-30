859f6a8fc064e86cf48146dab90fb79166e5612d
timmy-pelumy <timileyinpelumi.dev@gmail.com>
2022-01-30 13:13:54 +0100

    modified:   views/base.ejs

--- middlewares.js
+++ middlewares.js
@@ -1,13 +1,23 @@
 require('dotenv').config();
+var {Notification} = require('./models/transactions');
 
-function context(req, res, next) {
+async function context(req, res, next) {
 	res.locals.sitename = process.env.SITENAME;
 	res.locals.user = req.user || null;
 	res.locals.customer = req.user || {
 		lastLogin: {},
 	};
 
-	if (req.user) {
+	if (req.isAuthenticated) {
+		const updates = await Notification.find({
+			status: 'unread',
+			listener: req.user,
+		})
+			.lean()
+			.exec();
+
+		res.locals.updatesCount = updates.length === 0 ? '' : updates.length;
+
 		if (req.user.gender === 'male') {
 			res.locals.avatar = '/user_m.png';
 		} else if (req.user.gender === 'female') {
